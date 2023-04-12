646076f351b8f8ddcbcf927cc7081e0d3c818a62
seoyeomi <seoyeonii@ajou.ac.kr>
2023-04-12 23:34:17 +0900

    0412 push

--- screens/contents/recruit/Recruit.tsx
+++ screens/contents/recruit/Recruit.tsx
@@ -1,25 +1,68 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {Dimensions} from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  FlatList,
+} from 'react-native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {MainParamList} from '../../NavigationType';
+
+import {styles} from './Style';
+
 type RecruitScreenProps = {
   navigation: NativeStackNavigationProp<MainParamList, 'Recruit'>;
 };
 
+type ListUp = {
+  id: string;
+  title: string;
+  completed: boolean;
+};
+
+const initialData: ListUp[] = [
+  {id: '1', title: 'First', completed: false},
+  {id: '2', title: 'Second', completed: false},
+  {id: '3', title: 'Third', completed: false},
+  {id: '4', title: 'Fourth', completed: false},
+  {id: '5', title: 'Fifth', completed: false},
+  {id: '6', title: 'Sixth', completed: false},
+];
+
 const RecruitScreen = ({navigation}: RecruitScreenProps) => {
-  return (
+  const [data, setData] = useState<ListUp[]>(initialData);
+
+  const renderItem = ({item}: {item: ListUp}) => (
     <View>
-      <Text> Recruit Screen </Text>
+      <TouchableOpacity
+        style={[styles.item, item.completed && styles.completed]}
+        onPress={() => {
+          setData(
+            data.map(ListUp =>
+              ListUp.id === item.id
+                ? {...ListUp, completed: !ListUp.completed}
+                : ListUp,
+            ),
+          );
+        }}>
+        <Text style={styles.title}>{item.title} </Text>
+      </TouchableOpacity>
+    </View>
+  );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}> filter </Text>
+      <FlatList
+        style={styles.itemContainer}
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}></FlatList>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#FFD6BF',
-    flex: 1,
-  },
-});
 export default RecruitScreen;
