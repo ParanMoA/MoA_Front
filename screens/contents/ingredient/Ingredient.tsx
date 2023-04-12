46ba76c72563b27862b75d17f51a93a41d9d63ce
seonghwa <ghdtack@ajou.ac.kr>
2023-04-12 23:07:16 +0900

    카메라 Modal 구현

--- screens/contents/ingredient/Ingredient.tsx
+++ screens/contents/ingredient/Ingredient.tsx
@@ -1,33 +1,92 @@
-import React, {useEffect, useState} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  Text,
+  Button,
+  Platform,
+  ActionSheetIOS,
+  Pressable,
+} from 'react-native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {MainParamList} from '../../NavigationType';
 type IngredientScreenProps = {
   navigation: NativeStackNavigationProp<MainParamList, 'Ingredient'>;
 };
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-// import {
-//   Camera,
-//   CameraPermissionStatus,
-//   useCameraDevices,
-// } from 'react-native-vision-camera';
+import {
+  launchImageLibrary,
+  ImagePickerResponse,
+  launchCamera,
+} from 'react-native-image-picker';
+import UploadModeModal from './CameraModal';
 
 const IngredientScreen = ({navigation}: IngredientScreenProps) => {
-  // const [cameraPermission, setCameraPermission] =
-  //   useState<CameraPermissionStatus | null>(null);
+  const [response, setResponse] = useState<ImagePickerResponse | null>(null);
+  const [modalVisible, setModalVisible] = useState(false);
+  const onPickImage = (res: any) => {
+    if (res.didCancel || !res) {
+      return;
+    }
+    console.log(res);
+  };
 
-  // useEffect(() => {
-  //   const getCameraPermissionStatus = async () => {
-  //     const status = await Camera.getCameraPermissionStatus();
-  //     setCameraPermission(status);
-  //   };
-  //   getCameraPermissionStatus();
-  // }, []);
-  // const devices = useCameraDevices('wide-angle-camera');
-  // const device = devices.back;
+  const onLaunchCamera = () => {
+    launchCamera(
+      {
+        mediaType: 'photo',
+        maxWidth: 768,
+        maxHeight: 768,
+        includeBase64: Platform.OS === 'android',
+        saveToPhotos: true,
+      },
+      onPickImage,
+    );
+  };
+  const onLaunchImageLibrary = () => {
+    launchImageLibrary(
+      {
+        mediaType: 'photo',
+        maxWidth: 768,
+        maxHeight: 768,
+        includeBase64: Platform.OS === 'android',
+      },
+      onPickImage,
+    );
+  };
+
+  const modalOpen = () => {
+    if (Platform.OS === 'android') {
+      setModalVisible(true);
+    } else {
+      ActionSheetIOS.showActionSheetWithOptions(
+        {
+          options: ['카메라로 촬영하기', '사진 선택하기', '취소'],
+          cancelButtonIndex: 2,
+        },
+        buttonIndex => {
+          if (buttonIndex === 0) {
+            onLaunchCamera();
+          } else if (buttonIndex === 1) {
+            onLaunchImageLibrary();
+          }
+        },
+      );
+    }
+  };
+
   return (
-    <View>
-      <Text>No camera permission</Text>
+    <View style={styles.container}>
+      <Pressable onPress={modalOpen}>
+        <Icon name="plus" color="black" size={60} />
+      </Pressable>
+      <UploadModeModal
+        visible={modalVisible}
+        onClose={() => setModalVisible(false)}
+        onLaunchCamera={onLaunchCamera}
+        onLaunchImageLibrary={onLaunchImageLibrary}
+      />
     </View>
   );
 };
